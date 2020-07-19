9865c147b32b18bbb69b634be29ecd8173671a6d
Theodora <57870813+TheoRomanova@users.noreply.github.com>
2020-07-19 13:07:08 +0300

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Theodora on 7/19/20.
+//  Copyright © 2020 Feodora Andryieuskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
